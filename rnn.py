8c585a23e0522698c4d79bd742cf73228f93c50b
fcipollone <frank@DN0a626462.SUNet>
2017-05-17 10:04:53 -0700

    most updated code for model baseline

--- rnn.py
+++ rnn.py
@@ -10,7 +10,7 @@ class predictor:
 		self.num_features = 1
 		self.lr = 1e-4
 		self.lr2 = 1e-4
-		self.batch_size = 20
+		self.batch_size = 100
 		self.num_hidden = 100
 		self.num_classes = 7
 
@@ -20,6 +20,7 @@ class predictor:
 
 		self.loss = self.buildGraph()
 		self.addOptimizer()
+		self.merged = tf.summary.merge_all()
 
 	def createFeedDict(self, X, Y):
 		feedDict = {}
@@ -40,11 +41,21 @@ class predictor:
 
 	def buildModel(self):
 		batch_size = tf.shape(self.X)[0]
-		self.myGRU = tf.contrib.rnn.GRUCell(self.num_hidden,input_size=(None,self.timelength,self.num_features))
-		outputs, _ = tf.nn.dynamic_rnn(self.myGRU, self.X, initial_state = self.myGRU.zero_state(batch_size,tf.float32), scope='step1/rnn1')
-		self.secondGRU = tf.contrib.rnn.GRUCell(self.num_features,input_size=(None,self.timelength,self.num_hidden))
-		outputs, self.state = tf.nn.dynamic_rnn(self.secondGRU, outputs, initial_state = self.secondGRU.zero_state(batch_size,tf.float32),scope='step1/rnn2')
-		self.classification = tf.contrib.layers.fully_connected(self.state, num_outputs=self.num_classes, weights_initializer = tf.contrib.layers.xavier_initializer(), scope='step2')
+		self.myLSTM = tf.contrib.rnn.BasicLSTMCell(self.num_hidden)
+		#self.myGRU = tf.contrib.rnn.GRUCell(self.num_hidden,input_size=(None,self.timelength,self.num_features))
+		outputs, _ = tf.nn.dynamic_rnn(self.myLSTM, self.X, initial_state = self.myLSTM.zero_state(batch_size,tf.float32), scope='step1/rnn1')
+		self.secondLSTM = tf.contrib.rnn.BasicLSTMCell(self.num_features)
+		print self.myLSTM.state_size
+		print self.secondLSTM.state_size
+		#self.secondGRU = tf.contrib.rnn.GRUCell(self.num_features,input_size=(None,self.timelength,self.num_hidden))
+		outputs, self.state = tf.nn.dynamic_rnn(self.secondLSTM, outputs, initial_state = self.secondLSTM.zero_state(batch_size,tf.float32),scope='step1/rnn2')
+		self.state, _ = tf.split(self.state, [1,1], 0) # This is necessary when using the LSTM cell. splits tensor into sizes 1 and 1 on axis 0
+		self.state = tf.reshape(self.state, [-1,1])
+		packedOutputs = tf.stack(outputs)
+		#print packedOutputs.shape
+		outputs = tf.reshape(packedOutputs, [-1, self.timelength*self.num_features])
+		#print outputs.shape
+		self.classification = tf.contrib.layers.fully_connected(outputs, num_outputs=self.num_classes, weights_initializer = tf.contrib.layers.xavier_initializer(), scope='step2')
 		return self.state
 
 	def addLoss(self, y_out):
@@ -53,6 +64,7 @@ class predictor:
 			if 'weights:0' in el.name.split('/'):
 				l2_cost += tf.nn.l2_loss(el)*.01
 		self.lossWithoutReg = tf.reduce_mean(tf.losses.mean_squared_error(y_out,self.Y))*10000
+		tf.summary.scalar('Loss without regularization', self.lossWithoutReg)
 		self.secondLoss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.YClass, logits=self.classification))
 		return tf.reduce_mean(l2_cost) + self.lossWithoutReg
 
@@ -66,10 +78,12 @@ class predictor:
 
 	def train(self):
 		with tf.Session() as session:
+			train_writer = tf.summary.FileWriter('train', session.graph)
 			session.run(tf.global_variables_initializer())
 			for i in range(10000):
 				batch_x, batch_y = self.data.getBatchOf(size=self.batch_size, length=self.timelength)
-				session.run([self.optimizer], feed_dict=self.createFeedDict(batch_x, batch_y))
+				summary, _ = session.run([self.merged, self.optimizer], feed_dict=self.createFeedDict(batch_x, batch_y))
+				train_writer.add_summary(summary, i)
 				if i % 300 == 0:
 					print ("Iteration: ", i)
 					batch_x, batch_y = self.data.getBatchValid(size=self.batch_size, length=self.timelength)
