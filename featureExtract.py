fb52166782e399297ebc1df72f7d23da53f56a96
fcipollone <frank@DN0a626462.SUNet>
2017-05-17 08:33:19 -0700

    added the linear classifier from the hidden state to the classification of emotion

--- featureExtract.py
+++ featureExtract.py
@@ -7,20 +7,25 @@ from pyAudioAnalysis import audioFeatureExtraction
 class dataHolder:
 	def __init__(self):
 		self.dirs = ['DC', 'JE', 'JK', 'KL']
+		self.numbers = [15, 15, 15, 15, 30, 15, 15]
+		self.fileTypes = ['a', 'd', 'f', 'h', 'n', 'sa', 'su']
+		#self.numbers = [15]
+		#self.fileTypes = ['a']
 		filenames = self.getAllFilenames()
-		self.DC = self.getAllFeaures('data/DC',filenames)
-		self.DC, self.DCValid, self.DCtest = self.splitData(self.DC)
-		self.JE = self.getAllFeaures('data/JE',filenames)
-		self.JE, self.JEValid, self.JEtest = self.splitData(self.JE)
-		self.JK = self.getAllFeaures('data/JK',filenames)
-		self.JK, self.JKValid, self.JKtest = self.splitData(self.JK)
-		self.KL = self.getAllFeaures('data/KL',filenames)
-		self.KL, self.KLValid, self.KLtest = self.splitData(self.KL)
+		self.DC, self.DCLabels = self.getAllFeatures('data/DC',filenames)
+		print len(self.DC)
+		self.DC, self.DCValid, self.DCtest, self.DCLabels, self.DCValidLabels, self.DCtestLabels = self.splitData(self.DC, self.DCLabels)
+		self.JE, self.JELabels = self.getAllFeatures('data/JE',filenames)
+		self.JE, self.JEValid, self.JEtest, self.JELabels, self.JEValidLabels, self.JEtestLabels = self.splitData(self.JE, self.JELabels)
+		self.JK, self.JKLabels = self.getAllFeatures('data/JK',filenames)
+		self.JK, self.JKValid, self.JKtest, self.JKLabels, self.JKValidLabels, self.JKtestLabels = self.splitData(self.JK, self.JKLabels)
+		self.KL, self.KLLabels = self.getAllFeatures('data/KL',filenames)
+		self.KL, self.KLValid, self.KLtest, self.KLLabels, self.KLValidLabels, self.KLtestLabels = self.splitData(self.KL, self.KLLabels)
 
-	def splitData(self, data):
+	def splitData(self, data, dataLabels):
 		valid = int(.8*float(len(data)))
 		test = int(.9*float(len(data)))
-		return data[0:valid], data[valid:test], data[test:]
+		return data[0:valid], data[valid:test], data[test:], dataLabels[0:valid], dataLabels[valid:test], dataLabels[test:]
 
 	def getBatchOf(self, size, length, varname="DC"):
 		returnBatch = []
@@ -58,8 +63,57 @@ class dataHolder:
 			returnLabels.append(item[endind])
 		return np.array(returnBatch), np.array(returnLabels)
 
-	def getAllFeaures(self, dirname, filenames):
+	def getBatchWithLabels(self, size, length, varname="DC"):
+		returnBatch = []
+		returnLabels = []
+		labelsTaken = self.DCValidLabels
+		takenFrom = self.DCValid
+		if varname == 'JE':
+			labelsTaken = self.JEValidLabels
+			takenFrom = self.JEValid
+		elif varname == 'JK':
+			labelsTaken = self.JKValidLabels
+			takenFrom == self.JKValid
+		elif varname == 'KL':
+			labelsTaken = self.KLValidLabels
+			takenFrom = self.KLValid
+
+		for i in range(size):
+			index = random.randint(0,len(takenFrom)-1)
+			item = takenFrom[index]
+			endind = random.randint(length, len(item)-1)
+			startind = endind-length
+			returnBatch.append(item[startind:endind])
+			returnLabels.append(labelsTaken[index])
+		return np.array(returnBatch), np.array(returnLabels)
+
+	def getBatchWithLabelsValid(self, size, length, varname="DC"):
+		returnBatch = []
+		returnLabels = []
+		labelsTaken = self.DCLabels
+		takenFrom = self.DC
+		if varname == 'JE':
+			labelsTaken = self.JELabels
+			takenFrom = self.JE
+		elif varname == 'JK':
+			labelsTaken = self.JKLabels
+			takenFrom == self.JK
+		elif varname == 'KL':
+			labelsTaken = self.KLLabels
+			takenFrom = self.KL
+			
+		for i in range(size):
+			index = random.randint(0,len(takenFrom)-1)
+			item = takenFrom[index]
+			endind = random.randint(length, len(item)-1)
+			startind = endind-length
+			returnBatch.append(item[startind:endind])
+			returnLabels.append(labelsTaken[index])
+		return np.array(returnBatch), np.array(returnLabels)
+
+	def getAllFeatures(self, dirname, filenames):
 		returnList = []
+		returnLabels = []
 		indices = [i for i in range(0,9)]
 		indices.extend([i for i in range(22,34)])
 		indices = [1]
@@ -78,18 +132,30 @@ class dataHolder:
 			tot += np.mean(F[indices,:],axis=1)
 			num +=1 
 			returnList.append(F[indices,:].T)
+			if el[0] == 'a':
+				returnLabels.append(0)
+			elif el[0] == 'd':
+				returnLabels.append(1)
+			elif el[0] == 'f':
+				returnLabels.append(2)
+			elif el[0] == 'h':
+				returnLabels.append(3)
+			elif el[0] == 'n':
+				returnLabels.append(4)
+			elif el[0:2] == 'sa':
+				returnLabels.append(5)
+			else:
+				returnLabels.append(6)
 		print (tot/num)
-		return returnList
+		return returnList, returnLabels
 
 
 	def getAllFilenames(self):
-		fileTypes = ['a', 'd', 'f', 'h', 'n', 'sa', 'su']
-		numbers = [15, 15, 15, 15, 30, 15, 15]
 		#fileTypes = ['a']
 		#numbers = [5]
 		returnList = []
-		for i, el in enumerate(fileTypes):
-			for j in range(numbers[i]):
+		for i, el in enumerate(self.fileTypes):
+			for j in range(self.numbers[i]):
 				val = j+1
 				s = el
 				if val < 10:
--- rnn.py
+++ rnn.py
@@ -9,11 +9,14 @@ class predictor:
 		self.timelength = 15
 		self.num_features = 1
 		self.lr = 1e-4
+		self.lr2 = 1e-4
 		self.batch_size = 20
 		self.num_hidden = 100
+		self.num_classes = 7
 
 		self.X = tf.placeholder(tf.float32, [None, self.timelength, self.num_features])
 		self.Y = tf.placeholder(tf.float32, [None, self.num_features])
+		self.YClass = tf.placeholder(tf.int32, [None])
 
 		self.loss = self.buildGraph()
 		self.addOptimizer()
@@ -24,6 +27,12 @@ class predictor:
 		feedDict[self.Y] = Y
 		return feedDict
 
+	def createFeedDict2(self, X, Y):
+		feed_dict = {}
+		feed_dict[self.X] = X
+		feed_dict[self.YClass] = Y
+		return feed_dict
+
 	def buildGraph(self):
 		y_out = self.buildModel()
 		loss = self.addLoss(y_out)
@@ -32,9 +41,10 @@ class predictor:
 	def buildModel(self):
 		batch_size = tf.shape(self.X)[0]
 		self.myGRU = tf.contrib.rnn.GRUCell(self.num_hidden,input_size=(None,self.timelength,self.num_features))
-		outputs, _ = tf.nn.dynamic_rnn(self.myGRU, self.X, initial_state = self.myGRU.zero_state(batch_size,tf.float32), scope='rnn1')
+		outputs, _ = tf.nn.dynamic_rnn(self.myGRU, self.X, initial_state = self.myGRU.zero_state(batch_size,tf.float32), scope='step1/rnn1')
 		self.secondGRU = tf.contrib.rnn.GRUCell(self.num_features,input_size=(None,self.timelength,self.num_hidden))
-		outputs, self.state = tf.nn.dynamic_rnn(self.secondGRU, outputs, initial_state = self.secondGRU.zero_state(batch_size,tf.float32),scope='rnn2')
+		outputs, self.state = tf.nn.dynamic_rnn(self.secondGRU, outputs, initial_state = self.secondGRU.zero_state(batch_size,tf.float32),scope='step1/rnn2')
+		self.classification = tf.contrib.layers.fully_connected(self.state, num_outputs=self.num_classes, weights_initializer = tf.contrib.layers.xavier_initializer(), scope='step2')
 		return self.state
 
 	def addLoss(self, y_out):
@@ -43,11 +53,16 @@ class predictor:
 			if 'weights:0' in el.name.split('/'):
 				l2_cost += tf.nn.l2_loss(el)*.01
 		self.lossWithoutReg = tf.reduce_mean(tf.losses.mean_squared_error(y_out,self.Y))*10000
+		self.secondLoss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.YClass, logits=self.classification))
 		return tf.reduce_mean(l2_cost) + self.lossWithoutReg
 
 	def addOptimizer(self):
 		optimizer = tf.train.AdamOptimizer(self.lr)
-		self.optimizer = optimizer.minimize(self.loss)
+		step1Train = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,"step1")
+		self.optimizer = optimizer.minimize(self.loss, var_list=step1Train)
+		optimizer2 = tf.train.AdamOptimizer(self.lr2)
+		step2Train = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,"step2")
+		self.secondOptimizer = optimizer2.minimize(self.secondLoss, var_list=step2Train)
 
 	def train(self):
 		with tf.Session() as session:
@@ -59,10 +74,18 @@ class predictor:
 					print ("Iteration: ", i)
 					batch_x, batch_y = self.data.getBatchValid(size=self.batch_size, length=self.timelength)
 					loss, lwr, yo = session.run([self.loss, self.lossWithoutReg, self.state], feed_dict=self.createFeedDict(batch_x, batch_y))
-					print (yo)
 					print ("Loss = ", loss)
 					print ("Loss without regularization = ", lwr)
-					print batch_x[0]
+			for i in range(10000):
+				batch_x, batch_y = self.data.getBatchWithLabels(size=self.batch_size, length=self.timelength)
+				session.run([self.secondOptimizer], feed_dict=self.createFeedDict2(batch_x, batch_y))
+				if i % 100 == 0:
+					print ("Iteration: ", i)
+					batch_x, batch_y = self.data.getBatchWithLabelsValid(size=self.batch_size, length=self.timelength)
+					loss = session.run([self.secondLoss], feed_dict=self.createFeedDict2(batch_x, batch_y))
+					print ("Loss = ", loss)
+					#print ("Loss without regularization = ", lwr)
+
 
 p = predictor()
 p.train()
